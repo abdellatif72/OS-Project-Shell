a7e6aeeaf0d593c15e9594a084b610edf8d9d9b0
abdellatif72 <mmakw57@gmail.com>
2026-04-18 19:15:41 +0200

    implement pwd command with run, help, and destroy functions

--- Command.h
+++ Command.h
@@ -11,4 +11,5 @@ typedef struct Command
     // a simulation of abstract functions that will be overriden in other commands
     void (*run)(struct Command* self);
     void (*help)(struct Command* self);
+    void (*destroy)(struct Command* self);
 } Command;
---
+++ pwd.c
@@ -0,0 +1,56 @@
+#include "Command.h"
+#include <stdio.h>  // puts(), perror()
+#include <string.h> // strdup()
+#include <stdlib.h> // malloc()
+#include <unistd.h> // getcwd()
+
+static void run(Command *self)
+{
+    char buf[1024]; // buffer for current working directory
+
+    /*
+        getcwd(buffer, size of buffer)
+        if it fails, it returns NULL
+        otherwise, it store the string of the current working directory in the buffer
+    */
+    if (getcwd(buf, sizeof(buf)) != NULL)
+    {
+        puts(buf);
+    }
+    else
+    {
+        perror("Permission Denied");
+    }
+}
+
+static void help(Command *self)
+{
+    puts("pwd");
+    puts("Print the name of the current working directory.");
+}
+
+static void destroy(Command* self){
+    if(self==NULL)
+        return;
+
+    free(self->name);
+
+    if(self->args != NULL){
+        free(self->args);
+    }
+
+    free(self);
+}
+
+Command *pwd_command()
+{
+    Command *cmd = (Command *)malloc(sizeof(Command));
+
+    // since name in Command has no specific size, it is just a pointer that can point to any string, using it to point to "pwd", require allocating memory for that string using malloc, strdup() does that. No need to worry about what size to malloc and then copyting the text into it.
+    cmd->name = strdup("pwd");
+    cmd->args = NULL;
+    cmd->run = run;
+    cmd->help = help;
+
+    return cmd;
+}
