8e3a1db634525e7c63067709e0e44ae874775bad
abdellatif72 <mmakw57@gmail.com>
2026-04-18 18:00:00 +0200

    add Command structure and initial command files

---
+++ Command.h
@@ -0,0 +1,14 @@
+// to prevent multiple inclusion of this header file.
+#pragma once 
+
+// a simulation of an abstract class that will be used by all commands
+typedef struct Command
+{
+    char* name;
+    char** args;
+
+
+    // a simulation of abstract functions that will be overriden in other commands
+    void (*run)(struct Command* self);
+    void (*help)(struct Command* self);
+} Command;
